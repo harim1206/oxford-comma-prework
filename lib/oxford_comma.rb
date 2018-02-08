2917789bbd4c5becda7e149984b1d553a425be6d
Harim Choi <harim1206@gmail.com>
2018-02-08 21:06:24 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,8 @@ def oxford_comma(array)
 
   if array.size == 1
     return "#{array[0]}"
+  elsif array.size == 2
+    return "#{array[0]} and #{array[1]}."
   for i in 0..array.size-1 do
 
     if i == array.size-1
