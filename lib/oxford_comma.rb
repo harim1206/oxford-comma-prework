2dffc97b77cb1280c919d58ed58cb31b83f401cb
Harim Choi <harim1206@gmail.com>
2018-02-08 21:07:42 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,13 +6,13 @@ def oxford_comma(array)
     return "#{array[0]}"
   elsif array.size == 2
 
-    puts "#{array[0]} and #{array[1]}."
-    return "#{array[0]} and #{array[1]}."
+    puts "#{array[0]} and #{array[1]}"
+    return "#{array[0]} and #{array[1]}"
   else
     for i in 0..array.size-1 do
 
       if i == array.size-1
-        string += "and #{array[i]}."
+        string += "and #{array[i]}"
       else
         string += "#{array[i]}, "
       end
